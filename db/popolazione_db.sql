e73167239372c5de44cc9955a6c8002a133d3f63
Riccardo <riccardomedri9@gmail.com>
2023-12-07 17:13:50 +0100

    upgrade of repo

---
+++ db/popolazione_db.sql
@@ -0,0 +1,3 @@
+INSERT INTO `autore` (`idautore`, `username`, `password`, `nome`, `attivo`) VALUES
+(1, 'ginopino@blogtw.com', 'pass2019', 'Gino Pino', 1),
+(2, 'cippalippa@blogtw.com', 'pass2019', 'Cippa Lippa', 1);
--- routes/router-tessere.js
+++ routes/router-tessere.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const checkTessera = require('../pages/controller_tessere');
 
 const router = express.Router();
 
-router.get('/tessere', (req,res) => {
-    const filePath = path.join(__dirname, '../pages/tessere.json');
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if(err) {
-            console.error(err);
-            res.status(500).send('Errore durante lettura json');
-            return;
-        }
-
-        const jsonData = JSON.parse(data);
-
-        res.json(jsonData);
-    });
-});
+router.get('/tessera/:id', checkTessera.checkTessera);
 
 module.exports=router;
