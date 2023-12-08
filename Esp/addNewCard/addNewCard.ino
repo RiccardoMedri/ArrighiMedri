f19f0159ecc24ae879dccd5a1f5bfc927265964a
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-12-08 19:33:38 +0100

    modify to addNewCard.ino

--- Esp/addNewCard/addNewCard.ino
+++ Esp/addNewCard/addNewCard.ino
@@ -1,8 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid     = "testWifiSSID";
-const char* password = "testWifiSSID";
+#include <ESP8266HTTPClient.h>
+#include <WifiClient.h>
+
+const char* ssid     = "";
+const char* password = "";
 
 const char index_html[] PROGMEM = R"rawliteral(
     <!DOCTYPE HTML><html><head>
@@ -19,9 +22,10 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 ESP8266WebServer server(80);
 WiFiClient wifiClient;
-String urlDB = "http://X.X.X.X/addCard";
+String urlDB = "http://0.0.0.0/tessera/nuovaTessera";
 
 void setup() {
+  delay(6000);
   Serial.begin(115200);
   delay(10);
   connessione();
@@ -65,20 +69,23 @@ void handleCredential() {
   String ruolo = server.arg("ruolo");
 
   if(nome != "" && cognome != "" && ruolo != ""){
-    server.send(200, "text.html", addToDB(urlDB, nome, cognome, ruolo))
+    server.send(200, "text.html", addToDB(urlDB, nome, cognome, ruolo));
   }else{
     server.send(200, "text/html", "Form non compilato");
   }
 }
 
 String addToDB(String url, String nome, String cognome, String ruolo){
+
+  // scansione del nfc e recupero idtessera
+
   HTTPClient http;
-  String urlCompleto = url + "/" + nome + "-" + cognome + "-" + ruolo;
+  String urlCompleto = url + "/" + "000" + "-" + nome + "-" + cognome + "-" + ruolo;
   http.begin(wifiClient, urlCompleto.c_str());
   
-  int httResponseCode = http.GET()
+  int httpResponseCode = http.GET();
 
-    if (httpResponseCode > 0) {
+  if (httpResponseCode > 0) {
     String response = "";
     switch (httpResponseCode){
       case 200:
@@ -90,11 +97,13 @@ String addToDB(String url, String nome, String cognome, String ruolo){
       default:
         response = "codice di errore: " + String(httpResponseCode);
       break;
-    }
-    http.end();
-    Serial.println(response);
-    return response;
+  }
+  http.end();
+  Serial.println(response);
+  return response;
   }
   
   http.end();
+  
+  return "Response code < 0";
 }
--- webServer/db/creazione_db.sql
+++ webServer/db/creazione_db.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS `progettoIoT` DEFAULT CHARACTER SET utf8 ;
 USE `progettoIoT` ;
 
 CREATE TABLE IF NOT EXISTS `progettoIoT`.`tessere` (
-  `idtessera` INT NOT NULL,
+  `idtessera` VARCHAR NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `cognome` VARCHAR(512) NOT NULL,
   `ruolo` VARCHAR(45) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `progettoIoT`.`tessere` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `progettoIoT`.`ingressi` (
-  `idtessera` INT NOT NULL,
+  `idtessera` VARCHAR NOT NULL,
   `data` VARCHAR(100) NOT NULL,
   `orario` VARCHAR(512) NOT NULL,
   PRIMARY KEY (`idtessera`, `data`, `orario`))
